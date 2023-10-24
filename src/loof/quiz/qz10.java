9f5e754732422f6f14ff07b4f1803a162ef7f9a6
user <user@DESKTOP-P99H1R2>
2023-10-24 10:07:20 +0900

    update

--- src/loof/quiz/qz10.java
+++ src/loof/quiz/qz10.java
@@ -1,8 +1,15 @@
 package loof.quiz;
 
+//중첩 반복문을 사용해서 직각 삼각형을 그려보세요
 public class qz10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method
-}
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 }
--- src/loof/quiz/qz11.java
+++ src/loof/quiz/qz11.java
@@ -3,7 +3,16 @@ package loof.quiz;
 public class qz11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5-i; j++) {// 4 3 2 1 공백문자
+				System.out.print(" ");
+			}
+			for (int k = 1; k<= i; k++) { //1 2 3 4 5 출력 *
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
+// 1. 패턴분석 공백이 생기는가? , 잘못만들면 조건 검토
+// 2. 줄바꿈
